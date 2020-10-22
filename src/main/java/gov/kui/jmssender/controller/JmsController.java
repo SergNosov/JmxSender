93550ced14d0885f497b3b05cd5ea9cfa2d8aa08
Serg N <serg_vn@inbox.ru>
2020-10-22 17:56:59 +0900

    настройка html страниц

--- src/main/java/gov/kui/jmssender/controller/JmsController.java
+++ src/main/java/gov/kui/jmssender/controller/JmsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class JmsController {
 
-    @GetMapping("/sender")
+    @GetMapping({"/","/sender"})
     public String startPage(Model theModel){
         return "sender";
     }

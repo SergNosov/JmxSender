2c14b0059ec710918c7c30ec683582926af23f52
Serg N <serg_vn@inbox.ru>
2020-10-28 16:34:44 +0900

    добавление сообщения об ошибки валидации в sender.html

--- src/main/java/gov/kui/jmssender/controller/JmsController.java
+++ src/main/java/gov/kui/jmssender/controller/JmsController.java
@@ -4,6 +4,7 @@ import gov.kui.jmssender.model.DocumentDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -17,17 +18,21 @@ public class JmsController {
 
     private List<DocumentDto> documentDtoList = new ArrayList<>();
 
-    @GetMapping({"/","/sender"})
+    @GetMapping({"/","/showForm"})
     public String startPage(Model model){
-        model.addAttribute("newDocument", new DocumentDto());
+        model.addAttribute("documentDto", new DocumentDto());
         model.addAttribute("documentDtoList", documentDtoList);
         return "sender";
     }
 
-    @PostMapping("/submit")
-    public String  submitDocument(@Valid DocumentDto documentDto){
-        System.out.println("documentDto: "+ documentDto);
-        documentDtoList.add(documentDto);
-        return "senderOk";
+    @PostMapping("/showForm")
+    public String  submitDocument(@Valid DocumentDto documentDto, BindingResult bindingResult, Model model){
+
+        if (!bindingResult.hasErrors()) {
+            documentDtoList.add(documentDto);
+        }
+        model.addAttribute("documentDtoList", documentDtoList);
+
+        return "sender";
     }
 }

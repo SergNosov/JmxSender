a5dab1e7314a2eed33d8a2c13344bd8f94bbfc09
Serg N <serg_vn@inbox.ru>
2020-10-30 16:27:30 +0900

    refactoring HazelcastConfiguration

--- src/main/java/gov/kui/jmssender/config/HazelcastConfiguration.java
+++ src/main/java/gov/kui/jmssender/config/HazelcastConfiguration.java
@@ -1,7 +1,14 @@
 package gov.kui.jmssender.config;
 
+import com.hazelcast.collection.IList;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.ListConfig;
+
+import com.hazelcast.config.ManagementCenterConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import gov.kui.jmssender.model.DocumentDto;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,10 +16,21 @@ import org.springframework.context.annotation.Configuration;
 public class HazelcastConfiguration {
 
     @Bean
-    public Config hazelCastConfig(){
+    public Config hazelcastConfig(){
         Config config = new Config();
         config.setInstanceName("hazelcast-instance")
                 .addListConfig(new ListConfig().setName("documentDtoList"));
+      //  config.setManagementCenterConfig(new ManagementCenterConfig().setEnabled(true));
         return config;
+    }
+
+    @Bean
+    public HazelcastInstance hazelcastInstance(Config hazelcastConfig){
+        return Hazelcast.newHazelcastInstance(hazelcastConfig);
+    }
+
+    @Bean
+    public IList<DocumentDto> hazelcastDocumentDtoList(@Qualifier("hazelcastInstance") HazelcastInstance hazelcastInstance){
+        return hazelcastInstance.getList("documentDtoList");
     }
 }
--- src/main/java/gov/kui/jmssender/dao/impl/DocumentDtoRepositoryImpl.java
+++ src/main/java/gov/kui/jmssender/dao/impl/DocumentDtoRepositoryImpl.java
@@ -1,11 +1,9 @@
 package gov.kui.jmssender.dao.impl;
 
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IList;
+import com.hazelcast.collection.IList;
 import gov.kui.jmssender.dao.DocumentDtoRepository;
 import gov.kui.jmssender.model.DocumentDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,20 +11,20 @@ import java.util.List;
 @Service
 public class DocumentDtoRepositoryImpl implements DocumentDtoRepository {
 
-    private final HazelcastInstance hazelcastInstance;
+    private final IList<DocumentDto> hazelcastDocumentDtoList;
 
     @Autowired
-    public DocumentDtoRepositoryImpl(@Qualifier("hazelcastInstance") HazelcastInstance hazelcastInstance) {
-        this.hazelcastInstance = hazelcastInstance;
+    public DocumentDtoRepositoryImpl(IList<DocumentDto> hazelcastDocumentDtoList) {
+        this.hazelcastDocumentDtoList = hazelcastDocumentDtoList;
     }
 
     @Override
     public boolean addDto(DocumentDto documentDto) {
-        return hazelcastInstance.getList("documentDtoList").add(documentDto);
+        return hazelcastDocumentDtoList.add(documentDto);
     }
 
     @Override
     public List<DocumentDto> getAllDtos() {
-        return hazelcastInstance.getList("documentDtoList");
+        return hazelcastDocumentDtoList;
     }
 }

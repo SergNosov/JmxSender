241d79e92c5b50a1d1aeef6583b7384fa1523293
Serg N <serg_vn@inbox.ru>
2020-11-23 17:22:09 +0900

    изменение JmsProducerServiceImpl.isJmsAlive, применение RestTemplate с целью проверки доступности брокера сообщений ActiveMQ Artemis

--- src/main/java/gov/kui/jmssender/service/JmsProducerService.java
+++ src/main/java/gov/kui/jmssender/service/JmsProducerService.java
@@ -3,6 +3,16 @@ package gov.kui.jmssender.service;
 import gov.kui.jmssender.model.DocumentDto;
 
 public interface JmsProducerService {
+    /**
+     * Используется для отправки DTO-объекта брокером сообщений.
+     *
+     *@param documentDto DTO-объект для отправки.
+     */
     public void send(DocumentDto documentDto);
+
+    /**
+     * Используется для проверки доступности брокера сообщений.
+     *
+     */
     public void isJmsAlive();
 }
--- src/main/java/gov/kui/jmssender/service/impl/JmsProducerServiceImpl.java
+++ src/main/java/gov/kui/jmssender/service/impl/JmsProducerServiceImpl.java
@@ -6,8 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
 
 import javax.jms.Connection;
 import javax.jms.JMSException;
@@ -31,11 +37,34 @@ public class JmsProducerServiceImpl implements JmsProducerService {
     }
 
     public void  isJmsAlive(){
-        try (Connection con = jmsTemplate.getConnectionFactory().createConnection()){
-            log.info("--- con: "+con.getMetaData().getJMSProviderName());
-        } catch (JMSException jmsEx) {
-            log.error("--- jmsEx: "+jmsEx);
-            throw new RuntimeException(jmsEx);
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("origin", "http://localhost:8161");
+            httpHeaders.setOrigin("http://localhost:8161");
+            httpHeaders.setBasicAuth("root", "root");
+
+            ResponseEntity<String> responseEntity = restTemplate.exchange(
+                    "http://localhost:8161/console/jolokia/list",
+                    HttpMethod.GET,
+                    new HttpEntity<>(httpHeaders),
+                    String.class);
+
+            log.info("--- headers: " + httpHeaders);
+            log.info("--- origin: " + httpHeaders.getOrigin());
+            log.info("---status: " + responseEntity.getStatusCode());
+            log.info("--- !!!! " + responseEntity.getBody());
+        } catch (ResourceAccessException conEx){
+            log.info("--- Нет связи с брокером сообщений ActiveMQ Artemis: " + conEx.getMessage());
+            throw new RuntimeException("--- Нет связи с брокером сообщений ActiveMQ Artemis.");
         }
+
+//        try (Connection con = jmsTemplate.getConnectionFactory().createConnection()){
+//            log.info("--- con: "+con.getMetaData().getJMSProviderName());
+//        } catch (JMSException jmsEx) {
+//            log.error("--- jmsEx: "+jmsEx);
+//            throw new RuntimeException(jmsEx);
+//        }
     }
 }

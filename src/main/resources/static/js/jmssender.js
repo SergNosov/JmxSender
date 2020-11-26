30eecc5c860e0d4ab96917df8d73d8ceacf743a0
Serg N <serg_vn@inbox.ru>
2020-11-26 17:11:23 +0900

    jmssender.js создание функции добавления <input>

--- src/main/resources/static/js/jmssender.js
+++ src/main/resources/static/js/jmssender.js
@@ -5,3 +5,25 @@ function removeDiv() {
         statusBlock.parentElement.removeChild(statusBlock);
     }
 }
+
+function addSenderInput(){
+    console.log("-----click");
+    let parentDiv = document.getElementById("senders");
+    let childDiv = document.getElementsByClassName("oneSender").item(0).cloneNode(true);
+    parentDiv.append(childDiv);
+
+    // let div = document.createElement('div');
+    // div.className = "oneSender";
+    // div.className = "mt-10";
+    //
+    // let input = document.createElement('input');
+    // input.type = "text";
+    // input.className="form-control";
+    // input.required = true;
+    //
+    // div.append(input);
+    //
+    // console.log("----- div:"+div);
+    //parentDiv.append(div);
+
+}

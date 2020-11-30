5c6b694b424a26befb75c1357fad27a4bb1db42e
SergVN_Home <serg_vn@inbox.ru>
2020-11-30 22:09:14 +0900

    refactoring jmsSenderJq.js

--- src/main/resources/static/js/jmsSenderJq.js
+++ src/main/resources/static/js/jmsSenderJq.js
@@ -1,5 +1,4 @@
 $(function () {
-
     $('#btnAddSender').click(function () {
         let sendersDiv = $('#senders');
         let senderDiv = $('#sender0');
@@ -7,21 +6,12 @@ $(function () {
         const senderId = 'sender'+$('#senders .oneSender').length;
         let addingDiv = senderDiv.clone().attr('id', senderId);
 
-
         let buttonDelDiv = $('<div class="col-md-2">' +
             '<button type="button" class="btn-sm btn-secondary btnDeleteSender" onclick="btnDeleteSenderClick(this)">' +
             'Удалить' +
             '</button>' +
             '</div>'
         );
-        buttonDelDiv.attr('owner',senderId);
-
-        // buttonDelDiv.click(function () {
-        //    // let $this = $(this);
-        //     console.log("---!!!");
-        //     console.log("--- this:"+$this.attr('owner'));
-        //     $this.parent().remove($this);
-        // });
 
         buttonDelDiv.appendTo(addingDiv);
         addingDiv.appendTo(sendersDiv);
@@ -29,7 +19,5 @@ $(function () {
 });
 
 function btnDeleteSenderClick(element) {
-    console.log("---!!!");
-    console.log("--- this:"+element);
     $(element).parent().parent().remove();
 }
